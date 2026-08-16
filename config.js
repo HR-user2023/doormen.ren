bc202e1f05538ada908f79f675f95e39eb5764f4
HR-user2023 <zangela99809@gmail.com>
2026-08-16 14:06:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ⚠️ 只需要修改這一個檔案 ⚠️
 // 把下面的網址換成你自己在 Google Apps Script 部署後拿到的「網頁應用程式網址」
 // 網址長得像：https://script.google.com/macros/s/xxxxxxxxxxxxx/exec
-const APPS_SCRIPT_URL = "請貼上你的Apps Script網頁應用程式網址";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzZQ9NYrw-bm1Uw6Ljwg79lHeqmrT8oY_rN_lYm2421HHlyJgqNxYlTuOTqGhSqNl-0UA/exec";
