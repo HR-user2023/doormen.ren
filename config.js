54cda142098170a2233b0d4a8f7d2cd53bb9ffeb
HR-user2023 <zangela99809@gmail.com>
2026-09-07 15:39:11 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ⚠️ 只需要修改這一個檔案 ⚠️
 // 把下面的網址換成你自己在 Google Apps Script 部署後拿到的「網頁應用程式網址」
 // 網址長得像：https://script.google.com/macros/s/xxxxxxxxxxxxx/exec
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwOQAmXFHQ2X4T8YUxpJJZi9Q7xrT56R8UuU4EFERDRfGzIAjDDhE6twJtE4-SnTYD1bw/exec";
+const APPS_SCRIPT_URL = "請貼上你的Apps Script網頁應用程式網址";
